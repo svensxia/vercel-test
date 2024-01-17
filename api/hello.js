2a938e8ee81b33b327a258b5cac4b4b35f56ce4d
svensxia <svensxia@arcada.fi>
2024-01-17 11:26:44 +0200

    commit

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,19 @@
-// Filename is endpoint url name
+import { sql } from "@vercel/postgres"
 
-export default function handler(req, res){
+export default async function handler(req, res){
+
+    if (req.method == "POST"){
+        const {name, species, age} = req.body
+
+        await sql `INSERT INTO pets (
+            name, species, age
+        ) VALUES (
+            ${name}, ${species}, ${age}
+        )`
+    }
+
+    const {rows} = await sql `SELECT * FROM pets`
+
     res.status(200).json({
         method: req.method,
         hello: "World", 
--- api/initdb.js
+++ api/initdb.js
@@ -2,12 +2,15 @@ import { sql } from '@vercel/postgres';
  
 export default async function handler(res, req){
     try {
-    const result =
-    // Tagged template literal
-      await sql `CREATE TABLE IF NOT EXISTS pets (id serial, name varchar(255);`;
+        const result =
+        // Tagged template literal
+        await sql `CREATE TABLE IF NOT EXISTS pets (id serial, name varchar;`;
+        await sql `ALTER TABLE pets ADD COLUMN IF NOT EXISTS species varchar `;
+        await sql `ALTER TABLE pets ADD COLUMN IF NOT EXISTS age numeric `;
 
-    return res.status(200).json({ result });
-  } catch (error) {
-    return res.status(500).json({ error });
-  }
+        return res.status(200).json({ status: `success` });
+    } catch (error) {
+        return res.status(500).json({ error });
+        
+    }
 }
