706182223685ee1128e4459954890ef11449b208
svensxia <svensxia@arcada.fi>
2024-01-17 10:39:35 +0200

    fix

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,6 @@
 // Filename is endpoint url name
 export default function handler(req, res){
-    req.status(200).json({
+    res.status(200).json({
         method: req.method,
         hello: "World"
     })
